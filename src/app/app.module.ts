3a36ce3de31c0007c4623ab458536f763387b9a8
bobby <bobbyisbest3@gmail.com>
2018-09-22 15:17:28 -0400

    Adding routing and GameComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { GameComponent } from './game/game.component';
+
+const routes: Routes = [
+  { path: ``, redirectTo: `/game`, pathMatch: `full` },
+  { path: `game`, component: GameComponent }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GameComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
