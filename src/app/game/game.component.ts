ad2d09fea9d76544e56767ec20a9ac95d2277ff2
bobby <bobbyisbest3@gmail.com>
2018-09-22 17:12:47 -0400

    Adding canvas and asset loading

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,15 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, AfterViewInit, ViewChildren } from '@angular/core';
 
 @Component({
   selector: 'app-game',
   templateUrl: './game.component.html',
   styleUrls: ['./game.component.css']
 })
-export class GameComponent implements OnInit {
+export class GameComponent implements AfterViewInit {
+    objectKeys = Object.keys;
 
-  constructor() { }
+    @ViewChild('game') canvasRef: ElementRef;
+    @ViewChild('assets') assetsRef: ElementRef;
+    @ViewChild('background1') background1: ElementRef;
 
-  ngOnInit() {
-  }
+    numAssets: number;
+    loadedAssets: number;
+    context: CanvasRenderingContext2D;
+
+    constructor() { }
+
+    ngAfterViewInit(){
+        this.numAssets = this.assetsRef.nativeElement.childElementCount;
+        this.loadedAssets = 0;
+        this.context = this.canvasRef.nativeElement.getContext('2d');
+        console.log(this.numAssets + ` asset(s)`)
+    }
+
+    /**
+     * Called from HTML by each asset after it is done loading. Once the
+     * counter has determined all elements are loaded, it initializes
+     * the canvas.
+     */
+    assetLoaded(): void{
+        this.loadedAssets++;
+        console.log('Loaded asset')
+        if(this.loadedAssets == this.numAssets)
+            this.initCanvas();
+    }
+
+    initCanvas(){
+        this.context.drawImage(<HTMLImageElement>document.getElementById(`background1`), 0, 0, 800, 600);
+    }
 
 }
